5fc08411a26a48cf2d69a000ba6ceedb593b73d0
mzauchenberger <39142629+mzauchenberger@users.noreply.github.com>
2018-05-10 22:54:45 -0500

    mzauchenberger programmingassignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function, written by mzauchenberger, will return the inverse
+## of a matrix, using the cached result if it exists.
 
-## Write a short comment describing this function
+## The first function will both create/inverse the matrix and instill the possibility
+## of a cached return through the use of the <<- operator.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  z<- NULL
+  set<- function(y) {
+    x<<-y
+    z<<-NULL
+  }
+  get<- function() x
+  setinverse<- function(inverse) z<<- inverse
+  getinverse<- function() z
+  list(set = set, get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## The second function will attempt to compute the inverse matrix data and pull the cached data
+## if it exists in the global environment via the <<- operator.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  z<- x$getinverse()
+  if(!is.null(z)) {
+    message("getting cached data")
+    return(z)
+  }
+  data<- x$get()
+  z<- solve(data)
+  x$setinverse(z)
+  z
 }
